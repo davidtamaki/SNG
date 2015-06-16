92261bf1cff0febc57d4ad2b2a069d2578d0b2bd
davidtamaki <davidszajngarten@gmail.com>
2015-06-17 00:40:53 +0100

    update routes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, json
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +10,19 @@ def root():
 
 @app.route('/<path:path>')
 def static_proxy(path):
-	# send_static_file will guess the correct MIME type
+	# send_static_file default to 'static' for js folder
 	return app.send_static_file(path)
+
+
+@app.route('/uploads/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+    	filename, as_attachment=True)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
+
